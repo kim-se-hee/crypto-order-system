a01f791984a2867db6cb61ce8f3587769b02d0a9
kim-se-hee <saree98@naver.com>
2024-11-19 21:01:22 +0900

    chore: 최초 커밋

---
+++ src/main/java/ksh/example/mybit/MyBitApplication.java
@@ -0,0 +1,13 @@
+package ksh.example.mybit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ksh/example/mybit/MyBitApplicationTests.java
@@ -0,0 +1,13 @@
+package ksh.example.mybit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
