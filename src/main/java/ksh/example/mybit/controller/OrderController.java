37007225d2017e188cec0fe8cd9e450b271cc4df
kim-se-hee <saree98@naver.com>
2024-11-26 23:13:54 +0900

    feat: 주문 요청 작업을 컨트롤러에 매핑
    - 사용자 입력 정보를 OrderForm으로 받음
    - 폼 데이터의 검증
    - 주문 요청을 /order에 매핑

--- src/main/java/ksh/example/mybit/controller/OrderController.java
+++ src/main/java/ksh/example/mybit/controller/OrderController.java
@@ -1,2 +1,17 @@
-package ksh.example.mybit.controller;public class OrderController {
+package ksh.example.mybit.controller;
+
+import jakarta.validation.Valid;
+import ksh.example.mybit.controller.form.OrderForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderController {
+    @PostMapping("/order")
+    public void postOrder(@Valid @ModelAttribute OrderForm orderForm){
+
+    }
 }
--- src/main/java/ksh/example/mybit/controller/form/OrderForm.java
+++ src/main/java/ksh/example/mybit/controller/form/OrderForm.java
@@ -1,2 +1,32 @@
-package ksh.example.mybit.controller.form;public class OrderForm {
+package ksh.example.mybit.controller.form;
+
+import jakarta.validation.constraints.NotNull;
+import ksh.example.mybit.domain.OrderSide;
+import ksh.example.mybit.domain.OrderType;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+@Getter
+@Setter
+public class OrderForm {
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Long coinId;
+
+    @NotNull
+    @Range(min = 5000, max = 1000000000)
+    private Integer orderAmount;
+
+    @NotNull
+    private OrderSide orderSide;
+
+    @NotNull
+    private OrderType orderType;
+
+    private Integer limitPrice;
+
 }
