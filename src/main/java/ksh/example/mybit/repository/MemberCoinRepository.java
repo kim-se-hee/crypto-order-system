3df477cd8467ce77e4ec834ee8ae2784582217ff
kim-se-hee <saree98@naver.com>
2024-11-27 16:18:36 +0900

    feat: 주문 생성 로직 통합
    - 매수와 매도 주문 생성 메소드를 통합
    - 지갑 내 자산 현환 검증
    - 사용자 보유 원화 조회 기능 추가
    - 주문 생성

--- src/main/java/ksh/example/mybit/repository/MemberCoinRepository.java
+++ src/main/java/ksh/example/mybit/repository/MemberCoinRepository.java
@@ -4,7 +4,12 @@ import ksh.example.mybit.domain.Coin;
 import ksh.example.mybit.domain.Member;
 import ksh.example.mybit.domain.MemberCoin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberCoinRepository extends JpaRepository<MemberCoin, Long> {
     MemberCoin findByMemberAndCoin(Member member, Coin coin);
+
+    @Query("select mc from MemberCoin mc where mc.member = :member and mc.coin.ticker = 'won'")
+    MemberCoin findKoreanWonByMember(@Param("member") Member member);
 }
--- src/main/java/ksh/example/mybit/service/OrderService.java
+++ src/main/java/ksh/example/mybit/service/OrderService.java
@@ -1,10 +1,7 @@
 package ksh.example.mybit.service;
 
 import ksh.example.mybit.controller.form.OrderForm;
-import ksh.example.mybit.domain.Coin;
-import ksh.example.mybit.domain.Member;
-import ksh.example.mybit.domain.MemberCoin;
-import ksh.example.mybit.domain.Order;
+import ksh.example.mybit.domain.*;
 import ksh.example.mybit.repository.CoinRepository;
 import ksh.example.mybit.repository.MemberCoinRepository;
 import ksh.example.mybit.repository.MemberRepository;
@@ -22,23 +19,17 @@ public class OrderService {
     private final CoinRepository coinRepository;
     private final MemberCoinRepository memberCoinRepository;
 
-    public Order addSellOrder(OrderForm orderForm){
+    public Order addOrder(OrderForm orderForm){
         Long memberId = orderForm.getMemberId();
         Member member = checkMemberIsValid(memberId);
 
         Long coinId = orderForm.getCoinId();
-        Coin coin = checkCoinIsValid(coinId);
+        Coin coin = checkMarketSupports(coinId);
 
-        MemberCoin memberCoin = memberCoinRepository.findByMemberAndCoin(member, coin);
-        if(memberCoin == null)
-            throw new IllegalArgumentException("보유하고 있지 않은 코인입니다.");
-
-        Integer orderAmount = orderForm.getOrderAmount();
-        if(memberCoin.isLessThan(orderAmount))
-            throw new IllegalArgumentException("보유 수량보다 매도 수량이 많습니다.");
+        checkWallet(orderForm, member, coin);
 
         Order order = new Order(
-                orderAmount,
+                orderForm.getOrderAmount(),
                 orderForm.getOrderSide(),
                 orderForm.getOrderType(),
                 orderForm.getLimitPrice(),
@@ -55,11 +46,41 @@ public class OrderService {
         return optionalMember.get();
     }
 
-    private Coin checkCoinIsValid(Long coinId) {
+    private Coin checkMarketSupports(Long coinId) {
         Optional<Coin> optionalCoin = coinRepository.findById(coinId);
         if(optionalCoin.isEmpty())
             throw new IllegalArgumentException("존재하지 않는 코인입니다.");
         return optionalCoin.get();
+    }
+
+    private void checkWallet(OrderForm orderForm, Member member, Coin coin) {
+        OrderSide orderSide = orderForm.getOrderSide();
+        Integer orderAmount = orderForm.getOrderAmount();
+
+        if(orderSide == OrderSide.SELL){
+            checkCoinInWallet(orderAmount, member, coin);
+            return;
+        }
+
+        checkKoreanWonInWallet(member, orderAmount);
+    }
+
+    private void checkCoinInWallet(Integer orderAmount, Member member, Coin coin) {
+        MemberCoin memberCoin = memberCoinRepository.findByMemberAndCoin(member, coin);
+        if(memberCoin == null)
+            throw new IllegalArgumentException("보유하고 있지 않은 코인입니다.");
+
+        if(memberCoin.isLessThan(orderAmount))
+            throw new IllegalArgumentException("보유 수량보다 매도 수량이 많습니다.");
+    }
+
+    private void checkKoreanWonInWallet(Member member, Integer orderAmount) {
+        MemberCoin memberKoreanWon = memberCoinRepository.findKoreanWonByMember(member);
+        if(memberKoreanWon == null)
+            throw new IllegalArgumentException("원화가 부족합니다.");
+
+        if(memberKoreanWon.isLessThan(orderAmount))
+            throw new IllegalArgumentException("원화가 부족합니다.");
     }
 
 }
