3939d880719e3fa72f3f78298fc187bce07eb2ce
kim-se-hee <saree98@naver.com>
2024-12-01 19:04:29 +0900

    feat: 예외 처리 컨트롤러 구현

--- src/main/java/ksh/example/mybit/controller/ExceptionController.java
+++ src/main/java/ksh/example/mybit/controller/ExceptionController.java
@@ -1,4 +1,19 @@
 package ksh.example.mybit.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class ExceptionController {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> orderException(IllegalArgumentException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
