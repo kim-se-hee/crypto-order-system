7ed140b42d6ce46ad344b12f28c850ae60383d36
kim-se-hee <saree98@naver.com>
2024-12-15 17:49:45 +0900

    chore: 미사용 메소드 제거

--- src/main/java/ksh/example/mybit/repository/OrderRepository.java
+++ src/main/java/ksh/example/mybit/repository/OrderRepository.java
@@ -1,11 +1,7 @@
 package ksh.example.mybit.repository;
 
 import ksh.example.mybit.domain.Order;
-import ksh.example.mybit.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
-
-    Order findFirstByOrderStatusOrderByCreatedAtAsc(OrderStatus status);
-
 }
