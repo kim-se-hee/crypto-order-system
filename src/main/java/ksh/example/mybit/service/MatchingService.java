0215dd92704edf9218e82d6669714b09fe2cb261
kim-se-hee <saree98@naver.com>
2024-12-15 18:11:41 +0900

    feat: 시장가 포함 매칭 로직 구현
    - OrderReader를 통해 매칭될 두 주문을 조회
    - OrderMatcher를 통해 주문을 매칭
    - WalletUpdater를 통해 회원의 잔고 업데이트
    - Optional 안티 패턴 개선

--- src/main/java/ksh/example/mybit/service/MatchingService.java
+++ src/main/java/ksh/example/mybit/service/MatchingService.java
@@ -1,25 +1,31 @@
 package ksh.example.mybit.service;
 
+import ksh.example.mybit.domain.Order;
 import ksh.example.mybit.domain.Trade;
 import ksh.example.mybit.implementation.OrderMatcher;
+import ksh.example.mybit.implementation.OrderReader;
 import ksh.example.mybit.implementation.WalletUpdater;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class MatchingService {
 
+    private final OrderReader orderReader;
     private final OrderMatcher orderMatcher;
     private final WalletUpdater walletUpdater;
 
     @Transactional
-    public Optional<Trade> matchOrder() {
-        Optional<Trade> optionalTrade = orderMatcher.match();
-        optionalTrade.ifPresent(walletUpdater::updateWallet);
-        return optionalTrade;
+    public Trade matchOrder() {
+        Order order = orderReader.readMostPriorOrder();
+        Order matchingOrder = orderReader.readMatchingOrder(order);
+
+        Trade executedTrade = orderMatcher.match(order, matchingOrder);
+
+        walletUpdater.updateWallet(executedTrade);
+
+        return executedTrade;
     }
 }
