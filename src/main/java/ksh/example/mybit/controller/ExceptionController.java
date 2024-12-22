9b55950277026998092b2226423e3011260fcceb
kim-se-hee <saree98@naver.com>
2024-12-22 15:44:12 +0900

    fix: @RestController에서 @ControllerAdvice로 변경하여 api 예외 처리 버그 수정

--- src/main/java/ksh/example/mybit/controller/ExceptionController.java
+++ src/main/java/ksh/example/mybit/controller/ExceptionController.java
@@ -2,11 +2,12 @@ package ksh.example.mybit.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@ControllerAdvice
 public class ExceptionController {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
