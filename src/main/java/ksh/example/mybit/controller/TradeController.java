8cc978f4074af18d1ce53ad77c411011d1c6092f
kim-se-hee <saree98@naver.com>
2025-01-15 01:32:36 +0900

    refactor: 거래 내역 조회 url 변경 - memberId와 coinId를 쿼리 파라미터로 변경 - url이 자원 계층 구조 더 잘 드러내도록 변경

--- src/main/java/ksh/example/mybit/controller/TradeController.java
+++ src/main/java/ksh/example/mybit/controller/TradeController.java
@@ -2,12 +2,11 @@ package ksh.example.mybit.controller;
 
 import ksh.example.mybit.controller.dto.TradeHistoriesDto;
 import ksh.example.mybit.controller.dto.TradeHistoryDto;
+import ksh.example.mybit.service.MatchingService;
 import ksh.example.mybit.service.TradeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +16,8 @@ public class TradeController {
 
     private final TradeService tradeService;
 
-    @GetMapping("trade/history/member/{memberId}/coin/{coinId}")
-    public TradeHistoriesDto tradeHistoryOfCoin(@PathVariable Long memberId, @PathVariable Long coinId, Pageable pageable) {
+    @GetMapping("trade/history")
+    public TradeHistoriesDto tradeHistoryOfCoin(@RequestParam Long memberId, @RequestParam Long coinId, Pageable pageable) {
         List<TradeHistoryDto> histories = tradeService.getTradeHistory(memberId, coinId, pageable)
                 .stream()
                 .map(trade -> new TradeHistoryDto(trade, memberId))
@@ -26,5 +25,5 @@ public class TradeController {
 
         return new TradeHistoriesDto(histories);
     }
-
+    
 }
