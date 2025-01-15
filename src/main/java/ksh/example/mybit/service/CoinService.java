802af8955421e354b29e68bc2872ac66db47f8fe
kim-se-hee <saree98@naver.com>
2025-01-16 02:41:42 +0900

    feat: 상장 코인 조회 구현

--- src/main/java/ksh/example/mybit/service/CoinService.java
+++ src/main/java/ksh/example/mybit/service/CoinService.java
@@ -3,6 +3,9 @@ package ksh.example.mybit.service;
 import ksh.example.mybit.domain.Coin;
 import ksh.example.mybit.repository.CoinRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +16,9 @@ public class CoinService {
     public Coin findCoin(Long id) {
         return coinRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 코인입니다."));
+    }
+
+    public Page<Coin> getListedCoins(Pageable pageable) {
+        return coinRepository.findAll(pageable);
     }
 }
