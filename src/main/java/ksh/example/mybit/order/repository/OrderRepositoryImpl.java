c795f45e1cbc26db26642c9708f7a137ce446667
kim-se-hee <saree98@naver.com>
2025-01-21 20:26:35 +0900

    fix: 패키지 구조 변화에 따른 Q클래스 경로 조정

--- src/main/java/ksh/example/mybit/order/repository/OrderRepositoryImpl.java
+++ src/main/java/ksh/example/mybit/order/repository/OrderRepositoryImpl.java
@@ -8,17 +8,14 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.LockModeType;
 import ksh.example.mybit.coin.domain.Coin;
 import ksh.example.mybit.member.domain.Member;
-import ksh.example.mybit.order.domain.Order;
-import ksh.example.mybit.order.domain.OrderSide;
-import ksh.example.mybit.order.domain.OrderStatus;
-import ksh.example.mybit.order.domain.OrderType;
+import ksh.example.mybit.order.domain.*;
 import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
-import static ksh.example.mybit.domain2.QOrder.order;
+import static ksh.example.mybit.order.domain.QOrder.*;
 
 public class OrderRepositoryImpl implements OrderRepositoryCustom {
 
--- src/main/java/ksh/example/mybit/trade/repository/TradeRepositoryImpl.java
+++ src/main/java/ksh/example/mybit/trade/repository/TradeRepositoryImpl.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-import static ksh.example.mybit.domain2.QTrade.trade;
+import static ksh.example.mybit.trade.domain.QTrade.*;
 
 public class TradeRepositoryImpl implements TradeRepositoryCustom {
 
