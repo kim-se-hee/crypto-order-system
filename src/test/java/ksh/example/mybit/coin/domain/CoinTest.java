a23ceee2f7d8bf796000f261b6fe849671b8e71b
kim-se-hee <saree98@naver.com>
2025-02-13 18:54:54 +0900

    test: Coin 클래스 getChangeRate 단위 테스트 추가

--- src/test/java/ksh/example/mybit/coin/domain/CoinTest.java
+++ src/test/java/ksh/example/mybit/coin/domain/CoinTest.java
@@ -1,19 +1,17 @@
 package ksh.example.mybit.coin.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CoinTest {
 
     @DisplayName("현재가를 지정한 금액으로 업데이트 할 수 있다")
     @Test
-    void updatePrice(){
+    void updatePrice() {
         //given
         Coin coin = Coin.builder()
                 .price(BigDecimal.valueOf(10000))
@@ -25,6 +23,21 @@ class CoinTest {
 
         //then
         assertThat(coin.getPrice()).isEqualTo(price);
-     }
+    }
 
+    @DisplayName("전일 대비 상승률을 계산한다")
+    @Test
+    void getChangeRate() {
+        //given
+        Coin coin = Coin.builder()
+                .price(BigDecimal.valueOf(12000))
+                .closingPrice(BigDecimal.valueOf(10000))
+                .build();
+
+        //when
+        BigDecimal changeRate = coin.getChangeRate();
+
+        //then
+        assertThat(changeRate).isEqualTo(new BigDecimal("20.00"));
+    }
 }
