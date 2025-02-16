6a9bda65010c2a538799d740c7891b47b38fb68b
kim-se-hee <saree98@naver.com>
2025-02-16 20:41:07 +0900

    feat: 요청 정보 바인딩 예외 처리

--- src/main/java/ksh/example/mybit/global/exception/ExceptionController.java
+++ src/main/java/ksh/example/mybit/global/exception/ExceptionController.java
@@ -2,6 +2,7 @@ package ksh.example.mybit.global.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,4 +15,14 @@ public class ExceptionController {
     public ResponseEntity<String> orderException(IllegalArgumentException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<String> bindException(BindException e) {
+        return new ResponseEntity<>(
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
 }
--- src/main/java/ksh/example/mybit/member/dto/request/MemberJoinRequest.java
+++ src/main/java/ksh/example/mybit/member/dto/request/MemberJoinRequest.java
@@ -4,22 +4,22 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import ksh.example.mybit.member.service.dto.request.MemberJoinServiceRequest;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberJoinRequest {
 
-    @NotBlank
+    @NotBlank(message = "이메일은 필수입니다")
     @Email
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "비밀번호는 필수입니다")
     @Size(min = 6)
     private String password;
 
-    @NotBlank
+    @NotBlank(message = "회원 이름은 필수입니다")
     private String name;
 
     public MemberJoinServiceRequest toServiceRequest() {
@@ -29,4 +29,12 @@ public class MemberJoinRequest {
                 .name(name)
                 .build();
     }
+
+    @Builder
+    private MemberJoinRequest(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
 }
