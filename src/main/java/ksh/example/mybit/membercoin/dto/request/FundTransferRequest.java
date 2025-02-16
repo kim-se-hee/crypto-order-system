f452857e53180578c5b7ead17d802372729311da
kim-se-hee <saree98@naver.com>
2025-02-16 23:25:26 +0900

    feat: 필드 검증 오류 메세지 추가

--- src/main/java/ksh/example/mybit/membercoin/dto/request/FundTransferRequest.java
+++ src/main/java/ksh/example/mybit/membercoin/dto/request/FundTransferRequest.java
@@ -1,23 +1,27 @@
 package ksh.example.mybit.membercoin.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import ksh.example.mybit.membercoin.service.dto.request.FundTransferServiceRequest;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FundTransferRequest {
 
-    @NotNull
+    @NotNull(message = "회원 id는 필수입니다")
+    @Positive(message = "회원 id는 양의 정수입니다")
     private Long memberId;
 
-    @NotNull
+    @NotNull(message = "코인 id는 필수입니다")
+    @Positive(message = "코인 id는 양의 정수입니다")
     private Long coinId;
 
-    @NotNull
+    @NotNull(message = "수량은 필수입니다")
+    @Positive(message = "수량은 양수입니다")
     private BigDecimal quantity;
 
     public FundTransferServiceRequest toServiceRequest() {
@@ -28,4 +32,10 @@ public class FundTransferRequest {
                 .build();
     }
 
+    @Builder
+    private FundTransferRequest(Long memberId, Long coinId, BigDecimal quantity) {
+        this.memberId = memberId;
+        this.coinId = coinId;
+        this.quantity = quantity;
+    }
 }
--- src/main/java/ksh/example/mybit/membercoin/dto/request/InvestmentStaticsRequest.java
+++ src/main/java/ksh/example/mybit/membercoin/dto/request/InvestmentStaticsRequest.java
@@ -1,18 +1,21 @@
 package ksh.example.mybit.membercoin.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import ksh.example.mybit.membercoin.service.dto.request.InvestmentStaticsServiceRequest;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InvestmentStaticsRequest {
 
-    @NotNull
+    @Positive(message = "회원 id는 양의 정수입니다")
+    @NotNull(message = "회원 id는 필수입니다")
     private Long memberId;
 
-    @NotNull
+    @Positive(message = "코인 id는 양의 정수입니다")
+    @NotNull(message = "코인 id는 필수입니다")
     private Long coinId;
 
     public InvestmentStaticsServiceRequest toServiceRequest() {
@@ -20,5 +23,11 @@ public class InvestmentStaticsRequest {
                 .memberId(memberId)
                 .coinId(coinId)
                 .build();
+    }
+
+    @Builder
+    private InvestmentStaticsRequest(Long memberId, Long coinId) {
+        this.memberId = memberId;
+        this.coinId = coinId;
     }
 }
